977d79c08978e5f0486e8d73c8d6ac7656c1a670
Alumno <Alumno@POAPCA01LC0410.duoc.acad>
2026-06-22 22:06:55 -0400

    Agrego funciones

--- Funciones.py
+++ Funciones.py
@@ -1,1 +1,30 @@
 #Solo funciones
+#Función sin argumento y sin retorno
+
+#def funcion_sencilla():
+ #   print("Hola, soy una función sencilla.")
+
+#funcion_sencilla()
+
+
+
+
+#def suma():
+#    num1 = 3
+#    num2 = 5
+#    return(num1 + num2)
+# Llamando a la función, ejecuta todo el
+
+#print("la suma es: ", suma())
+
+
+def suma(a,b):
+    sumar = a + b
+    print(f"La suma de los argumentos es: {sumar}")
+num1 = int(input("Ingrese primer número: "))
+num2 = int(input("Ingrese segundo número: "))
+suma(num1,num2)
+
+
+
+
